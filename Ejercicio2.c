ef67a8b72cc9bc3e92623c31949e398cee588ae3
camilaAA6 <322213997a@gmail.com>
2025-04-02 08:51:39 -0600

    Create Ejercicio2.c

---
+++ Ejercicio2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int main n,i;
+    printf("Ingrese un numero entero\n");
+    scanf("%d" &n);
+do{
+    printf("%d\n",i);
+    i++;
+} while(i<=n);
+return 0;
+}
